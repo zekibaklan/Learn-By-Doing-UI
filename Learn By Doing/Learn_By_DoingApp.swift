6225310a2c74e1c36b51179f4d4c994959b80c07
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-09-14 12:19:55 +0300

    Initial Commit

---
+++ Learn By Doing/Learn_By_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_By_DoingApp.swift
+//  Learn By Doing
+//
+//  Created by Zeki Baklan on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Learn_By_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
